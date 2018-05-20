892181a1db87579df23ff523d6d430ae51cb8851
Josh Cesana <joshuacesana@gmail.com>
2018-05-20 19:32:49 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,6 +5,10 @@ def key_for_min_value(name_hash)
   key_for_min_value = ""
   min_value = ""
   
+  if name_hash == {}
+    return nil 
+  end 
+  
   name_hash.each do |this_key, this_value|
     if key_for_min_value == ""
       key_for_min_value = this_key
